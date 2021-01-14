b8989b823fe434c520c9b8f2e72608c06dda94cf
novaesbezerra <novaesbezerra@gmail.com>
2021-01-14 21:53:27 -0200

    Iniciando GET medicos

--- T3/agenda/src/main/java/br/ufscar/dc/dsw/controller/MedicoController.java
+++ T3/agenda/src/main/java/br/ufscar/dc/dsw/controller/MedicoController.java
@@ -1,8 +1,13 @@
 package br.ufscar.dc.dsw.controller;
 
+import java.io.IOException;
+import java.util.List;
+
 import javax.validation.Valid;
 
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -10,19 +15,60 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import br.ufscar.dc.dsw.domain.Medico;
+import br.ufscar.dc.dsw.domain.Paciente;
 import br.ufscar.dc.dsw.service.spec.IMedicoService;
+import br.ufscar.dc.dsw.service.spec.IUsuarioService;
 
 @Controller
-@RequestMapping("/medicos")
+@RestController()
 public class MedicoController {
 
 	@Autowired
 	private IMedicoService service;
 
+	@Autowired
+	private IUsuarioService userService;
+	
+	private boolean isJSONValid(String jsonInString) {
+		try {
+			return new ObjectMapper().readTree(jsonInString) != null;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+	
+	private void parse(Medico medico, JSONObject json) {
+
+		Object id = json.get("id");
+		if (id != null) {
+			if (id instanceof Integer) {
+				medico.setId(((Integer) id).longValue());
+			} else {
+				medico.setId((Long) id);
+			}
+		}
+
+		medico.setNome((String) json.get("nome"));
+		medico.setCrm((String) json.get("crm"));
+	}
+	
+	// GET http://localhost:8081/medicos/
+		@GetMapping(path = "/medicos")
+		public ResponseEntity<List<Medico>> lista() {
+			List<Medico> lista = service.buscarTodos();
+			if (lista.isEmpty()) {
+				return ResponseEntity.notFound().build();
+			}
+			return ResponseEntity.ok(lista);
+		}
+		
 	@GetMapping("/cadastrar")
 	public String cadastrar(Medico medico) {
 		return "medico/cadastro";
